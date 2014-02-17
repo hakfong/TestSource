2670f34e70aaa4304b3e5de0e5ed073410509e7e
hakfong <fong_fy@yahoo.com>
2014-02-17 14:11:45 +0700

    Car

---
+++ TestGit/TestGit/Car.h
@@ -0,0 +1,13 @@
+//
+//  Car.h
+//  TestGit
+//
+//  Created by Mapring on 2/17/14.
+//  Copyright (c) 2014 Mapring. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Car : NSObject
+
+@end
---
+++ TestGit/TestGit/Car.m
@@ -0,0 +1,13 @@
+//
+//  Car.m
+//  TestGit
+//
+//  Created by Mapring on 2/17/14.
+//  Copyright (c) 2014 Mapring. All rights reserved.
+//
+
+#import "Car.h"
+
+@implementation Car
+
+@end
