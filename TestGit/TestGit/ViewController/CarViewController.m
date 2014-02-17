7b9a95eef4e89bd70de25236e06c72d1fe9322f9
hakfong <fong_fy@yahoo.com>
2014-02-17 14:13:37 +0700

    CarViewController

---
+++ TestGit/TestGit/ViewController/CarViewController.m
@@ -0,0 +1,38 @@
+//
+//  CarViewController.m
+//  TestGit
+//
+//  Created by Mapring on 2/17/14.
+//  Copyright (c) 2014 Mapring. All rights reserved.
+//
+
+#import "CarViewController.h"
+
+@interface CarViewController ()
+
+@end
+
+@implementation CarViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
