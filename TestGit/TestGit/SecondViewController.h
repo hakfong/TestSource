a6d55dc1cb93ae5167b706a8bdf650872bd43b19
hakfong <fong_fy@yahoo.com>
2014-02-17 10:30:45 +0700

    Send data from viewVC to other

---
+++ TestGit/TestGit/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  TestGit
+//
+//  Created by Mapring on 2/17/14.
+//  Copyright (c) 2014 Mapring. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface SecondViewController : ViewController
+
+@end
