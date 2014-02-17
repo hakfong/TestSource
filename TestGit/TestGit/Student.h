965ca4963ec027dafdf87aa112c2e09fd2aa9c52
hakfong <fong_fy@yahoo.com>
2014-02-17 09:55:03 +0700

    Student

---
+++ TestGit/TestGit/Student.h
@@ -0,0 +1,13 @@
+//
+//  Student.h
+//  TestGit
+//
+//  Created by Mapring on 2/17/14.
+//  Copyright (c) 2014 Mapring. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Student : NSObject
+
+@end
